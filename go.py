a89d24656e963552f5aec0d20c0de7cfd9bdf9a4
Serbirial <gabesummers0@gmail.com>
2019-04-22 19:05:01 -0500

    Update go.py

--- go.py
+++ go.py
@@ -2,8 +2,5 @@ from mpl import player
 import os, sys
 
 if os.name != "nt": print("\033]2;Media player : idle\007")
-else:
-	sys.stdout.write("Mpl has many bugs on windows")
-	sys.stdout.flush()
 
 player.start()

cf90dfb653da6a3d8363827b2beda4041542f4e9
Serbirial <gabesummers0@gmail.com>
2019-04-25 03:03:31 -0500

    Update other.py
    Added page support

--- mpl/other.py
+++ mpl/other.py
@@ -4,22 +4,43 @@ import sqlite3
 class Helper:
     def __init__(self):
         pass
-    def get_songs(self):
+
+    def pages(self, num=False):
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+        pages = cur.execute("SELECT page FROM songs").fetchall()
+        if not num:
+            string = f'There are {len(set(pages))} pages and {len(pages)} songs'
+        elif num is True:
+            return len(set(pages))
+        con.close()
+        return string
+
+    def get_songs(self, page=0):
         returned = ''
         con = sqlite3.connect('database.db')
         cur = con.cursor()  # handles requests made to the database
         # check for songs
-        cur.execute('SELECT song,author,length,id FROM songs')
-        rows = cur.fetchall()
-        if len(rows) != 0:
-            pass
-        elif len(rows) == 0:
-            returned += "No songs found"
-        for song, author, length, id in cur.execute(
-                'SELECT song,author,length,id FROM songs'):
-            returned += f'{id} -> {song} - {author}: {length}\n'
-        con.close()
-        return returned
+        if page is None:
+            rows = cur.execute(
+                'SELECT song,author,length,id FROM songs').fetchall()
+            if len(rows) == 0:
+                returned += "No songs found"
+            for song, author, length, id in cur.execute(
+                    'SELECT song,author,length,id FROM songs'):
+                returned += f'{id} -> {song} - {author}: {length}\n'
+            con.close()
+            return returned            
+        elif type(page) == int:
+            rows = cur.execute(
+                'SELECT song,author,length,id FROM songs WHERE page=?', (page,)).fetchall()
+            if len(rows) == 0:
+                returned += "No songs found"
+            for song, author, length, id in cur.execute(
+                    'SELECT song,author,length,id FROM songs WHERE page=?', (page,)):
+                returned += f'{id} -> {song} - {author}: {length}\n'
+            con.close()
+            return returned
 
     def get_song_from_id(self, id):
         con = sqlite3.connect('database.db')

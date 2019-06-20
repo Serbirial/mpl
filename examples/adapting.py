c13c667e9f6c13de5f547a00a33cf2eaf77c4a69
Serbirial <gabesummers0@gmail.com>
2019-06-19 20:13:19 -0500

    Create adapting.py

---
+++ examples/adapting.py
@@ -0,0 +1,34 @@
+colors = {
+    "other": "",
+    "other2": "",
+    "main": "",
+    "main2": "",
+    "volume": "",
+    "volume2": "",
+    "repeat": "",
+    "repeat2": "",
+    "keys": "",
+    "keys2": "",
+    "playing": "\033[01;34m",
+    "playing2": "\033[00m",
+    "time": "\033[01;32m",
+    "time2": "\033[00m",
+    "input_arrow": "\033[01;34m",
+    "input_arrow2": "\033[00m",
+}
+
+
+
+# Here is where the chars are inserted
+"""
+{theme.colors["other"]}     {cache["other"]}               {theme.colors["other2"]}
+{theme.colors["main"]}      {cache["main"]}                {theme.colors["main2"]}
+{theme.colors["volume"]}    Volume -> {cache["volume"]}    {theme.colors["volume2"]}
+{theme.colors["repeat"]}    Repeating -> {cache["repeat"]} {theme.colors["repeat2"]}
+{theme.colors["keys"]}      {cache["keys"]}                {theme.colors["keys2"]}
+{theme.colors["playing"]}   {cache["playing"]}             {theme.colors["playing2"]}
+
+{theme.colors["time"]} {cache["time"]} {theme.colors["time2"]}
+"""
+# key is inserted before
+# key2 is inserted after
